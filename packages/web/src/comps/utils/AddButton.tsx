8aecd464c779d99ffd3e830eaf5f695558c612d9
Joy Richmond <joy.richmond@wwt.com>
2019-11-10 17:16:29 -0600

    create AddButton comp

---
+++ packages/web/src/comps/utils/AddButton.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const AddButton = () => {
+  <button className="iconButton">
+    <FontAwesomeIcon icon={['fas', 'plus']} />
+  </button>;
+};
+
+export default AddButton;
