5f3e8c50be192f386a82cca6e27b9f944b8a877a
Joy Richmond <joy.richmond@wwt.com>
2019-11-10 17:16:18 -0600

    add fontawesome library to project

--- packages/web/src/App.js
+++ packages/web/src/App.js
@@ -2,17 +2,14 @@ import React from 'react';
 import './App.css';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 import spendingTrackerReducer from './reducers/spendingTrackerReducer';
+import './fontawesome';
 
 import Header from './comps/Header';
 import AtAGlance from './comps/AtAGlance';
 import SpendingTracker from './comps/SpendingTracker';
 
 const store = createStore(spendingTrackerReducer);
-
-library.add(faChevronDown);
 
 function App() {
   return (
---
+++ packages/web/src/fontawesome.js
@@ -0,0 +1,9 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faChevronDown,
+  faPlus,
+  faTrashAlt,
+  faPencilAlt,
+} from '@fortawesome/free-solid-svg-icons';
+
+library.add(faChevronDown, faPlus, faTrashAlt, faPencilAlt);
