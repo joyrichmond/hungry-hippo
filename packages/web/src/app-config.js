4d0a36f0d1b24e9ee27fdc23760fb65e64145bbc
Joy Richmond <joy.richmond@wwt.com>
2020-01-15 00:00:15 -0600

    code cleanup

--- packages/web/src/app-config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-  ENTER_KEY: 13,
-  SPACE_KEY: 32,
-  TAB_KEY: 9,
-};
-
-export default config;
--- packages/web/src/fontawesome.ts
+++ packages/web/src/fontawesome.ts
@@ -1,4 +1,4 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faChevronDown, faPlus, faTrashAlt, faPencilAlt, faArrowRight, faTimes, faDoorOpen, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faArrowRight, faChevronDown, faDoorOpen, faPencilAlt, faPlus, faTimes, faTrashAlt, faUser } from '@fortawesome/free-solid-svg-icons';
 
 library.add(faChevronDown, faPlus, faTrashAlt, faPencilAlt, faArrowRight, faTimes, faDoorOpen, faUser);
