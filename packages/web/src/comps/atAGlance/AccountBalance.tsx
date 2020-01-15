3f932dcf930577c3cd9fa5ca83e157d25187b035
Joy Richmond <joy.richmond@wwt.com>
2020-01-14 23:16:38 -0600

    delete unused files

--- packages/web/src/comps/atAGlance/AccountBalance.tsx
+++
@@ -1,9 +0,0 @@
-import React, { FC } from 'react';
-
-type Props = {};
-
-const AccountBalance: FC<Props> = () => {
-  return <div></div>;
-};
-
-export default AccountBalance;
--- packages/web/src/comps/atAGlance/atAGlanceLineItemComponent/LineItem.tsx
+++
@@ -1,9 +0,0 @@
-import React, { FC } from 'react';
-
-type Props = {};
-
-const LineItem: FC<Props> = () => {
-  return <div></div>;
-};
-
-export default LineItem;
